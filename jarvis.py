00443cf9ecc3e589d72161246d9ebc7ed875261b
Deep Sarkar <deepsarkar18877@gmail.com>
2024-02-11 21:06:12 +0530

    added youtube player

--- jarvis.py
+++ jarvis.py
@@ -5,6 +5,10 @@ import datetime
 import os
 import cv2
 import random
+import wikipedia
+import webbrowser
+import pywhatkit as pwt
+from youtubesearchpython import VideosSearch
 from requests import get
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -69,7 +73,8 @@ if __name__ == '__main__':
     # speak("Good Morning Sir, the code is working!")
     while True:
         query = takecommand().lower()
-        #logic building for tasks
+#logic building for tasks
+        
         if "open notepad" in query:
             npath = "C:\\Program Files\WindowsApps\\Microsoft.WindowsNotepad_11.2312.18.0_x64__8wekyb3d8bbwe\\Notepad\\Notepad.exe"
             os.startfile(npath)
@@ -80,7 +85,8 @@ if __name__ == '__main__':
         
         if "please stop" in query:
             break
-        #music section
+
+#play the music 
 
         if "play music" in query:
             music_dir = "D:\\music"
@@ -93,10 +99,29 @@ if __name__ == '__main__':
             rd = random.choice(songs)
             os.startfile(os.path.join(music_dir,rd))
 
+#show your ip address
+            
         elif "show me ip address" in query:
             ip = get('https://api.ipify.org').text
             speak(f"your IP Address is {ip}")
-            
+
         elif "give news" in query:
             news()
 
+#search anything on wikipedia
+            
+        elif "wikipedia" in query:
+            speak("Searching in wikipedia...")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query,sentences = 1)
+            speak("according to wikipedia")
+            speak(results)
+        
+#Play videos on youtube
+            
+        elif "play song on youtube" in query:
+            # webbrowser.open("www.youtube.com")
+            speak("What should i search sir?")
+            cm = takecommand().lower()
+            # webbrowser.open(f"{cm}")
+            pwt.playonyt(f"{cm}")
