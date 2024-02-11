0f8bfd1f042c69a9161e63bfa2370a43035fe2eb
Deep Sarkar <deepsarkar18877@gmail.com>
2024-02-11 13:07:27 +0530

    updated wish()

--- jarvis.py
+++ jarvis.py
@@ -1,25 +1,26 @@
 import pyttsx3
 import speech_recognition as sr
+import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 
 engine.setProperty('voice', voices[1].id)
 
-#Speak Function for Friday Assistant
+#converting text to speech
 def speak(audio):
     engine.say(audio)
     print(audio)
     engine.runAndWait()
 
-#Speech Recognizer Function for Friday Assistant
+#converting voice to text
     
 def takecommand():
     r= sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
-        audio = r.listen(source, timeout=1 , phrase_time_limit=5)
+        audio = r.listen(source, timeout=5 , phrase_time_limit=5)
 
     try:
         print("Recognizing...")
@@ -31,7 +32,21 @@ def takecommand():
         return "none"
     return query
 
+#wish function
+
+def wish():
+    hour =  int(datetime.datetime.now().hour)
+    # print(hour)
+    
+    if hour >= 0 and hour <=12:
+        speak("Good Morning Sir")
+    elif hour >12 and hour <=15:
+        speak("Good Afternoon Sir")
+    elif hour >15 and hour<=24:
+        speak("Good Night Sir")
+
 if __name__ == '__main__':
+    wish()
     takecommand()
     speak("Good Morning Sir, the code is working!")
     
